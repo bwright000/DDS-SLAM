7b67b45c10088383d5fa8ea5511b52374ec0a07f
bwright000 <benjamin.wright.25@ucl.ac.uk>
2026-04-15 19:55:37 +0100

    Update scene rep to prevent pixels being discarded.
    Co-Authored-By: bwright000 <238664802+bwright000@users.noreply.github.com>

--- model/scene_rep.py
+++ model/scene_rep.py
@@ -390,14 +390,14 @@ class JointEncoding(nn.Module):
                 edge_semantic_loss = compute_loss(
                     rend_dict["edge_semantic"].squeeze()[valid_depth_mask],
                     target_edge_semantic.squeeze()[valid_depth_mask],
-                    UsePercentage=notFirstMap
+                    UsePercentage=False
                 )
             else:
                 edge_semantic_loss = compute_loss(
                     rend_dict["edge_semantic"].squeeze()[valid_depth_mask],
                     target_edge_semantic.squeeze()[valid_depth_mask],
                     border=border.squeeze()[valid_depth_mask],
-                    UsePercentage=notFirstMap
+                    UsePercentage=False
                 )
 
             if 'rgb0' in rend_dict:
